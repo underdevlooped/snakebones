44d01e6e5f1c738abd1e7b0784b2772c1c6e5838
kern.andre <kern.andre.pro@gmail.com>
2018-09-11 12:08:59 -0300

    @underdevlooped
    Initial commit (teste)

---
+++ dijksra02.py
@@ -0,0 +1,127 @@
+"""
+https://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm/
+
+Created on Sat Jul 21 11:41:04 2018
+
+sabado, 21 de julho de 2018, 11:41:04
+.
+@author: Andre Kern
+
+
+Below are the detailed steps used in Dijkstra’s algorithm to find the shortest path from a single source vertex to all other vertices in the given graph.
+Algorithm
+1) Create a set sptSet (shortest path tree set) that keeps track of vertices included in shortest path tree, i.e., whose minimum distance from source is calculated and finalized. Initially, this set is empty.
+2) Assign a distance value to all vertices in the input graph. Initialize all distance values as INFINITE. Assign distance value as 0 for the source vertex so that it is picked first.
+3) While sptSet doesn’t include all vertices
+….a) Pick a vertex u which is not there in sptSet and has minimum distance value.
+….b) Include u to sptSet.
+….c) Update distance value of all adjacent vertices of u. To update the distance values, iterate through all adjacent vertices. For every adjacent vertex v, if sum of distance value of u (from source) and weight of edge u-v, is less than the distance value of v, then update the distance value of v.
+
+Notes:
+1) The code calculates shortest distance, but doesn’t calculate the path information. We can create a parent array, update the parent array when distance is updated (like prim’s implementation) and use it show the shortest path from source to different vertices.
+
+2) The code is for undirected graph, same dijkstra function can be used for directed graphs also.
+
+3) The code finds shortest distances from source to all vertices. If we are interested only in shortest distance from the source to a single target, we can break the for the loop when the picked minimum distance vertex is equal to target (Step 3.a of the algorithm).
+
+4) Time Complexity of the implementation is O(V^2). If the input graph is represented using adjacency list, it can be reduced to O(E log V) with the help of binary heap. Please see
+Dijkstra’s Algorithm for Adjacency List Representation for more details.
+
+5) Dijkstra’s algorithm doesn’t work for graphs with negative weight edges. For graphs with negative weight edges, Bellman–Ford algorithm can be used, we will soon be discussing it as a separate post.
+
+
+Python program for Dijkstra's single 
+source shortest path algorithm. The program is 
+for adjacency matrix representation of the graph
+"""
+ 
+# Library for INT_MAX
+import sys
+ 
+class Graph():
+ 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)] 
+                      for row in range(vertices)]
+ 
+    def printSolution(self, dist):
+        print "Vertex tDistance from Source"
+        for node in range(self.V):
+            print node,"t",dist[node]
+ 
+    # A utility function to find the vertex with 
+    # minimum distance value, from the set of vertices 
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
+ 
+        # Initilaize minimum distance for next node
+        min = sys.maxint
+ 
+        # Search not nearest vertex not in the 
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+ 
+        return min_index
+ 
+    # Funtion that implements Dijkstra's single source 
+    # shortest path algorithm for a graph represented 
+    # using adjacency matrix representation
+    def dijkstra(self, src):
+ 
+        dist = [sys.maxint] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+ 
+            # Pick the minimum distance vertex from 
+            # the set of vertices not yet processed. 
+            # u is always equal to src in first iteration
+            u = self.minDistance(dist, sptSet)
+ 
+            # Put the minimum distance vertex in the 
+            # shotest path tree
+            sptSet[u] = True
+ 
+            # Update dist value of the adjacent vertices 
+            # of the picked vertex only if the current 
+            # distance is greater than new distance and
+            # the vertex in not in the shotest path tree
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and sptSet[v] == False and
+                   dist[v] > dist[u] + self.graph[u][v]:
+                        dist[v] = dist[u] + self.graph[u][v]
+ 
+        self.printSolution(dist)
+ 
+# Driver program
+g  = Graph(9)
+g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+           [4, 0, 8, 0, 0, 0, 0, 11, 0],
+           [0, 8, 0, 7, 0, 4, 0, 0, 2],
+           [0, 0, 7, 0, 9, 14, 0, 0, 0],
+           [0, 0, 0, 9, 0, 10, 0, 0, 0],
+           [0, 0, 4, 14, 10, 0, 2, 0, 0],
+           [0, 0, 0, 0, 0, 2, 0, 1, 6],
+           [8, 11, 0, 0, 0, 0, 1, 0, 7],
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]
+          ];
+ 
+g.dijkstra(0);
+ 
+
+Output:
+Vertex   Distance from Source
+0                0
+1                4
+2                12
+3                19
+4                21
+5                11
+6                9
+7                8
+8                14
