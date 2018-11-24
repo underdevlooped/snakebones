74ca746463867d81d76afbdbcc7540d7a480e12f
kern.andre <kern.andre.pro+gitcommit@gmail.com>
2018-11-24 18:02:05 -0200

    teste criado 10 hosts e 10 hubs com sucesso

--- gnsbinder.py
+++ gnsbinder.py
@@ -950,13 +950,13 @@ def main():
     # pprint(pc.version)
     # pprint(pc.computes)
     # pprint(pc.projects)
-    nodes = ('10.0.10.1', '10.0.10.2', '10.0.10.3')
+    nodes = ('10.0.10.1', '10.0.10.2', '10.0.10.3', '10.0.10.4', '10.0.10.5', '10.0.10.6', '10.0.10.7', '10.0.10.8', '10.0.10.9', '10.0.10.10')
     new_hub = set_hub()
     # breakpoint()
-    pprint(pc.nodes(project_id=project_id, new=new_hub))
-    # for nodeip in nodes:
-    #     new_node = set_node(nodeip, '24')
-    #     pprint(pc.nodes(project_id=project_id, new=new_node))
+    for nodeip in nodes:
+        new_node = set_node(nodeip, '24')
+        pprint(pc.nodes(project_id=project_id, new=new_node))
+        pprint(pc.nodes(project_id=project_id, new=new_hub))
 
 # curl -X POST 192.168.139.1:3080/v2/projects/389dde3d-08ac-447b-8d54-b053a3f6ed19/nodes -d '{"name": "VPCS 1", "node_type": "vpcs", "compute_id": "vm"}'
 
